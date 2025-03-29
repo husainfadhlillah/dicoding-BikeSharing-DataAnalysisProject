53b525fa0f8cff54b157e415a2e36fa522f74ad7
MUHAMMAD HUSAIN FADHLILLAH <husainfadhlillah01@gmail.com>
2025-03-29 16:08:22 +0700

    Update dashboard.py

--- submission/dashboard/dashboard.py
+++ submission/dashboard/dashboard.py
@@ -30,8 +30,12 @@ st.markdown(
 # Fungsi untuk memuat data
 @st.cache_data
 def load_data():
-    hour_data = pd.read_csv('dashboard/hour_data_clean.csv')
-    day_data = pd.read_csv('dashboard/day_data_clean.csv')
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    hour_data_path = os.path.join(script_dir, 'hour_data_clean.csv')
+    day_data_path = os.path.join(script_dir, 'day_data_clean.csv')
+    
+    hour_data = pd.read_csv(hour_data_path)
+    day_data = pd.read_csv(day_data_path)
     
     # Konversi kolom tanggal ke datetime
     hour_data['dteday'] = pd.to_datetime(hour_data['dteday'])
